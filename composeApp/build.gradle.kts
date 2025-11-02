81fd6ac314472d7bb46767ac1e9cc1023a06de5a
Smponi <Philipp@smponias.de>
2025-11-02 21:04:40 +0100

    Baue Navigation3 ein

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,6 +43,7 @@ kotlin {
             implementation(libs.androidx.activity.compose)
             implementation(libs.koin.android)
             implementation(libs.koin.androidx.compose)
+            implementation(libs.androidx.navigation3.ui)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -54,6 +55,8 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
 
+
+            implementation(libs.androidx.navigation3.runtime)
             implementation(libs.koin.compose)
             implementation(libs.koin.compose.viewmodel)
             api(libs.koin.core)
--- composeApp/src/commonMain/kotlin/com/mouse/mouse/App.kt
+++ composeApp/src/commonMain/kotlin/com/mouse/mouse/App.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
@@ -36,9 +37,9 @@ fun App() {
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
-                Column(
+                Row (
                     modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
                     Text("Compose: $greeting")
