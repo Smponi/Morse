ad335236c4cf4d7b87f8cea2cd55445f6e452ba5
Smponi <Philipp@smponias.de>
2025-11-16 11:56:12 +0100

    CI/CD

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     kotlin("plugin.serialization") version "2.0.21"
-    id("org.jetbrains.kotlinx.kover") version "0.8.3"
+    id("org.jetbrains.kotlinx.kover") version "0.9.3"
 }
 
 kover {
